6cd48f0aa01578d68cc13c4b7e5bfbe2be8a3d13
Höhl, Lukas <lukas.hoehl@accso.de>
2023-02-13 17:28:06 +0100

    ci: move apko and melange config into root folder
    Signed-off-by: Höhl, Lukas <lukas.hoehl@accso.de>

--- scripts/fuse-fs-build.sh
+++ scripts/fuse-fs-build.sh
@@ -8,19 +8,16 @@ if [ ! -f ${DIR}/../melange.rsa ]; then
   docker run --rm -v "${DIR}/../":/work distroless.dev/melange keygen
 fi
 docker run --privileged --rm -v "${DIR}/../":/work \
-  distroless.dev/melange build deploy/fuse-fs/melange.yaml \
+  distroless.dev/melange build melange.fuse-fs.yaml \
   --arch amd64 \
   --repository-append packages \
-  --signing-key melange.rsa \
-  --source-dir /work
+  --signing-key melange.rsa
 
 # Your GitHub username
 GITHUB_USERNAME="hown3d"
 REF="ghcr.io/${GITHUB_USERNAME}/s3-csi/fuse-fs"
 
-
-
 docker run --rm -w /work -v "${DIR}/../":/work \
-  distroless.dev/apko build deploy/fuse-fs/apko.yaml \
+  distroless.dev/apko build apko.fuse-fs.yaml \
   "${REF}" /work/packages/fuse-fs-output.tar -k  melange.rsa.pub \
   --build-arch amd64
