3bd3ac2990288926520be9daea4f4b3c102d8b8f
Höhl, Lukas <lukas.hoehl@accso.de>
2023-02-25 18:37:57 +0100

    chore(fuse-pod-manager): deploy script
    Signed-off-by: Höhl, Lukas <lukas.hoehl@accso.de>

--- scripts/build-with-ko.sh
+++ scripts/build-with-ko.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && /bin/pwd )"
 
 REPO=hown3d/s3-csi
 export KO_DOCKER_REPO=${KO_DOCKER_REPO_PREFIX}/$REPO
 for img in $IMAGES; do
-  MAIN="./cmd/$img"
+  MAIN="$DIR/../cmd/$img"
   TAG=$(ko build $MAIN)
   pushd manifests/overlays/minikube
   kustomize edit set image ghcr.io/$REPO/$img=$(echo $TAG | sed "s/$(minikube ip)/localhost/")
---
+++ scripts/fuse-pod-manager/deploy.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && /bin/pwd )"
+
+IMAGES=fuse-pod-manager KO_DOCKER_REPO_PREFIX=$(minikube ip):5000 $DIR/../build-with-ko.sh
+
+kustomize build $DIR/../../manifests/overlays/minikube/ | kubectl apply -f -
